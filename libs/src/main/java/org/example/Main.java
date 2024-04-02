96ac9c4a4e5aece72ba93903cc73d953fe6951c8
Bastien <bamar@gatling.io>
2024-04-02 17:01:59 +0200

    playing with multimodule

---
+++ libs/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.apache.commons.logging.LogSource;
+
+public class Main {
+
+    public static String COUCOU="COUCOU";
+    
+    public static void main(String[] args) {
+        LogSource s;
+        System.out.println("Hello world!");
+    }
+}
---
+++ load-testing-submodule/src/main/java/org/sample/Main.java
@@ -0,0 +1,10 @@
+//import org.apache.commons.logging.LogSource;
+package org.sample;
+
+public class Main {
+    public static String HELLO = org.example.Main.COUCOU;
+    public static void main(String args[]) {
+        //LogSource s;
+	System.out.println(org.example.Main.COUCOU);
+    }
+}
